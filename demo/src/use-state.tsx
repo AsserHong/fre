7d06f460d545a9944c98527c1cf2b59ffad3980e
Asser <286357736@qq.com>
2023-02-07 23:59:25 +0800

    feature: add child componnet for App & update .gitignore

--- demo/src/use-state.tsx
+++ demo/src/use-state.tsx
@@ -4,10 +4,26 @@ function App() {
   const [list, setList] = useState([1, 2, 3])
   return (
     <div>
+      <Children />
       {list.map((d) => (
         <span>{d}</span>
       ))}{' '}
       <button onClick={() => setList(list.concat(4))}>+</button>
+    </div>
+  )
+}
+
+function Children() {
+  const [num, setNum] = useState(0);
+
+  return (
+    <div onClick={() => setNum(num + 1)}>
+      <div>
+        this is a child component
+      </div>
+      <span>
+        get number: { num }
+      </span>
     </div>
   )
 }
